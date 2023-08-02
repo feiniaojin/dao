8ca3e6c3b4462074e3a750fe75ac2e714226e468
qinyujie <943868899@qq.com>
2023-08-02 14:16:27 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,115 @@
+export default {
+  title: "大道之行",
+  description: "读书笔记",
+  base: "/",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    socialLinks: [
+      { icon: "github", link: "https://github.com/feiniaojin/dao" },
+    ],
+    footer: {
+      copyright: "Copyright © 2020-present Qin Yujie",
+    },
+    nav: [
+      {
+        text: "大道之行",
+        link: "/0/0.0.md",
+        activeMatch: "/",
+      },
+      {
+        text: "悟道DDD",
+        link: "http://ddd.feiniaojin.com/",
+      }
+    ],
+    sidebar: [
+      {
+        items: [
+          {
+            text: "README",
+            link: "/0/0.0",
+          },
+        ],
+      },
+      {
+        text: "前言",
+        collapsible: true,
+        items: [
+          {
+            text: "作者简介",
+            link: "/0/author",
+          },
+          {
+            text: "全文导读",
+            link: "/0/0.3",
+          },
+        ],
+      },
+      {
+        text: "楞严经",
+        collapsible: true,
+        items: [
+          {
+            text: "卷一",
+            link: "/LengYanJing/卷一",
+          },
+          {
+            text: "卷二",
+            link: "/LengYanJing/卷二",
+          },
+        ],
+      },
+      {
+        text: "药师经",
+        collapsible: true,
+        items: [
+          {
+            text: "药师经",
+            link: "/2/2.1",
+          }
+        ],
+      },
+      {
+        text: "金刚经",
+        collapsible: true,
+        items: [
+          {
+            text: "金刚经",
+            link: "/3/3.1",
+          }
+        ],
+      },
+      {
+        text: "地藏经",
+        collapsible: true,
+        items: [
+          {
+            text: "金刚经",
+            link: "/4/4.1",
+          }
+        ],
+      },
+      {
+        text: "易经",
+        collapsible: true,
+        items: [
+          {
+            text: "易经",
+            link: "/5/5.1",
+          }
+        ],
+      },
+      {
+        text: "道德经",
+        collapsible: true,
+        items: [
+          {
+            text: "道德经",
+            link: "/6/6.1",
+          }
+        ],
+      }
+    ],
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+title: 大道之行
+layout: home
+hero:
+  name: 大道之行
+  text: 知易行难
+  tagline: 观天之道，执天之行
+  actions:
+    - theme: brand
+      text: 开始阅读 →
+      link: /0/0.0
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/feiniaojin/dao
+
+features:
+  - title: 读书笔记
+    details: 经典著作的读书笔记
+  - title: 个人理解
+    details: 基于原文加入笔者的个人理解
+---
